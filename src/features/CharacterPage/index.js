7063ceab89b72539cd9242f2cf28ff2235a18b79
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-15 21:46:53 +0200

    example data in character details added

--- src/features/CharacterPage/index.js
+++ src/features/CharacterPage/index.js
@@ -1,15 +1,32 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { fetchCharacterDetails } from "./characterDetailsSlice";
+import { StatusChecker } from "../../common/StatusChecker";
+import {
+  fetchCharacterDetails,
+  selectCharacterDetails,
+  selectCharacterStatus,
+} from "./characterDetailsSlice";
 
 export const CharacterPage = () => {
   const dispatch = useDispatch();
+
   const { id } = useParams();
+
+  const character = useSelector(selectCharacterDetails);
+  const status = useSelector(selectCharacterStatus);
 
   useEffect(() => {
     dispatch(fetchCharacterDetails(id));
   }, [id, dispatch]);
 
-  return <>Character {id}</>;
+  return (
+    <StatusChecker status={status}>
+      <p>Name: {character.name || "Unknown"}</p>
+      <p>Status: {character.status || "Unknown"}</p>
+      <p>Species: {character.species || "Unknown"}</p>
+      <p>Type: {character.type || "Unknown"}</p>
+      <p>Gender: {character.gender || "Unknown"}</p>
+    </StatusChecker>
+  );
 };

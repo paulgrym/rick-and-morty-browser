078369b36d15dd2baaa1bc7ec5dcde526b3cd6ec
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-15 21:38:27 +0200

    character details slice created and added to store

--- src/core/store.js
+++ src/core/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { charactersReducer } from "../features/CharactersPage/charactersSlice";
+import { characterDetailsReducer } from "../features/CharacterPage/characterDetailsSlice";
 import rootSaga from "./rootSaga";
 import createSagaMiddleware from "redux-saga";
 
@@ -8,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     characters: charactersReducer,
+    characterDetails: characterDetailsReducer,
   },
   middleware: [sagaMiddleware],
 });
---
+++ src/features/CharacterPage/characterDetailsSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const characterDetailsSlice = createSlice({
+  name: "characterDetails",
+  initialState: {
+    characterDetails: [],
+    status: "initial",
+  },
+
+  reducers: {
+    fetchCharacterDetails: (state) => {
+      state.status = "loading";
+    },
+
+    fetchCharacterDetailsSuccess: (state, { payload: characterDetails }) => {
+      state.status = "success";
+      state.characterDetails = characterDetails;
+    },
+
+    fetchCharacterDetailsError: (state) => {
+      state.status = "error";
+    },
+  },
+});
+
+export const {
+  fetchCharacterDetails,
+  fetchCharacterDetailsSuccess,
+  fetchCharacterDetailsError,
+} = characterDetailsSlice.actions;
+
+const selectCharacter = (state) => state.characterDetails;
+
+export const selectCharacterDetails = (state) =>
+  selectCharacter(state).characterDetails;
+export const selectCharacterStatus = (state) => selectCharacter(state).status;
+
+export const characterDetailsReducer = characterDetailsSlice.reducer;

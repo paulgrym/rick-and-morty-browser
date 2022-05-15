95ab3b42893495fb47e59f0d1a462601696d6fa9
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-15 21:39:34 +0200

    fetchCharacterDetails dispatch implemented in CharacterPage

--- src/features/CharacterPage/index.js
+++ src/features/CharacterPage/index.js
@@ -1,12 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchCharacterDetails } from "./characterDetailsSlice";
 
 export const CharacterPage = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const { id } = useParams();
 
-  // useEffect(() => {
-  //   dispatch(fetchCharacter(id));
-  // }, [id, dispatch]);
+  useEffect(() => {
+    dispatch(fetchCharacterDetails(id));
+  }, [id, dispatch]);
 
   return <>Character {id}</>;
 };

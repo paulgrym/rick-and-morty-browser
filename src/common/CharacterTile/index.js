72448f4d1f23251986a8ac7812000c216a58887e
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-16 12:30:21 +0200

    CharacterTile styles added

--- src/common/CharacterTile/index.js
+++ src/common/CharacterTile/index.js
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { StyledLink, ListItem, Image, Title } from "./styled";
 
 export const CharacterTile = ({ id, name, image }) => {
   return (
-    <Link to={`/characters/${id}`}>
-      <li>
-        <img src={image} alt="Character portrait" /> {name}
-      </li>
-    </Link>
+    <ListItem>
+      <StyledLink to={`/characters/${id}`}>
+        <Image src={image} alt="Character portrait" />
+        <Title> {name}</Title>
+      </StyledLink>
+    </ListItem>
   );
 };
---
+++ src/common/CharacterTile/styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const ListItem = styled.li`
+  text-align: center;
+  padding: 16px;
+  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
+  border-radius: 5px;
+  transition: transform 0.5s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  gap: 16px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Image = styled.img`
+  border-radius: 5px;
+`;
+
+export const Title = styled.p`
+  margin: 0;
+`;

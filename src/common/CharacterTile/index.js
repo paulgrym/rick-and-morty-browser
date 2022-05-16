3362f144d270d4cf074121ded7b24a2663f15b74
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-16 23:17:31 +0200

    Button added to CharacterTile

--- src/common/CharacterTile/index.js
+++ src/common/CharacterTile/index.js
@@ -1,12 +1,14 @@
 import { StyledLink, ListItem, Image, Title } from "./styled";
+import { Button } from "../Button";
 
-export const CharacterTile = ({ id, name, image }) => {
+export const CharacterTile = ({ id, name, image, onButtonClick, content }) => {
   return (
     <ListItem>
       <StyledLink to={`/characters/${id}`}>
         <Image src={image} alt="Character portrait" />
         <Title> {name}</Title>
       </StyledLink>
+      <Button onButtonClick={onButtonClick} content={content} />
     </ListItem>
   );
 };

fc5ab56e0416ff6d962d5fe79e5029b81deb232b
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-16 23:19:24 +0200

    FavouriteCharactersList component updated

--- src/features/characters/FavouriteCharactersList/index.js
+++ src/features/characters/FavouriteCharactersList/index.js
@@ -1,8 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import { CharacterTile } from "../../../common/CharacterTile";
+import { ListWrapper } from "../../../common/ListWrapper";
+import { Section } from "../../../common/Section";
+import { Title } from "../../../common/Title";
+import {
+  removeCharacterFromFavourites,
+  selectFavouritesCharacters,
+} from "../CharactersList/charactersListSlice";
+
 export const FavouriteCharactersList = () => {
+  const dispatch = useDispatch();
+  const favouriteCharacters = useSelector(selectFavouritesCharacters);
+
   return (
-    <>
-      <h1>Favourite</h1>
-      <p>Test</p>
-    </>
+    <Section>
+      <Title>Favourite</Title>
+      <ListWrapper>
+        {favouriteCharacters.map((character) => (
+          <CharacterTile
+            key={character.id}
+            id={character.id}
+            name={character.name}
+            image={character.image}
+            onButtonClick={() =>
+              dispatch(removeCharacterFromFavourites(character.id))
+            }
+            content="Remove from favourites"
+          />
+        ))}
+      </ListWrapper>
+    </Section>
   );
 };

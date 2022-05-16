22ad8a8a35f76e08b88c8580cbc6e8ec7bd78a9f
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-05-16 13:46:46 +0200

    Section and Title components created

---
+++ src/common/Title/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  color: ${({ theme }) => theme.colors.title};
+  font-size: 36px;
+  line-height: 1.2;
+  margin: 56px 0 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+    font-size: 28px;
+    margin: 32px 0 16px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+    text-align: center;
+  }
+`;
--- src/features/characters/CharactersList/index.js
+++ src/features/characters/CharactersList/index.js
@@ -2,7 +2,9 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CharacterTile } from "../../../common/CharacterTile";
 import { ListWrapper } from "../../../common/ListWrapper";
+import { Section } from "../../../common/Section";
 import { StatusChecker } from "../../../common/StatusChecker";
+import { Title } from "../../../common/Title";
 import {
   fetchCharactersList,
   selectCharactersList,
@@ -20,17 +22,20 @@ export const CharactersList = () => {
   console.log(characters);
 
   return (
-    <StatusChecker status={status}>
-      <ListWrapper>
-        {characters.map((character) => (
-          <CharacterTile
-            key={character.id}
-            id={character.id}
-            name={character.name}
-            image={character.image}
-          />
-        ))}
-      </ListWrapper>
-    </StatusChecker>
+    <Section>
+      <StatusChecker status={status}>
+        <Title>Characters</Title>
+        <ListWrapper>
+          {characters.map((character) => (
+            <CharacterTile
+              key={character.id}
+              id={character.id}
+              name={character.name}
+              image={character.image}
+            />
+          ))}
+        </ListWrapper>
+      </StatusChecker>
+    </Section>
   );
 };
